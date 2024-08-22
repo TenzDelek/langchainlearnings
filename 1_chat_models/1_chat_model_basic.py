e59ef266507b3c1226391738888bda445abfd66a
TenzDelek <tibetdelek@gmail.com>
2024-08-22 21:54:44 +0530

    updated chatmodel input

--- 1_chat_models/1_chat_model_basic.py
+++ 1_chat_models/1_chat_model_basic.py
@@ -4,9 +4,9 @@ from langchain_google_genai import ChatGoogleGenerativeAI
 load_dotenv()
 
 model = ChatGoogleGenerativeAI(model="gemini-1.5-pro")
+taken=input("Enter your query: ")
+res=model.invoke(taken)
 
-res=model.invoke("what is the capital of tibet")
-
-print("result: ")
-print(res)
+# print("result: ")
+# print(res)
 print(f'content only {res.content}')
