31fdc3ab10fcd54a5e0b2f10d5eefca795c954fc
TenzDelek <tibetdelek@gmail.com>
2024-08-23 16:32:18 +0530

    textspliter options and recursivesplit

---
+++ 4_rag/2c_recursivesplit.py
@@ -0,0 +1,82 @@
+import os
+import chardet
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_community.document_loaders import TextLoader
+from langchain_community.vectorstores import Chroma
+from langchain_google_genai import GoogleGenerativeAIEmbeddings 
+
+# Define the directory containing the text file and the persistent directory
+current_dir = os.path.dirname(os.path.abspath(__file__))
+file_path = os.path.join(current_dir, "books", "odyssey.txt")
+persistent_directory = os.path.join(current_dir, "db", "chroma_db_recursivesplit")
+
+# Check if the Chroma vector store already exists
+if not os.path.exists(persistent_directory):
+    print("Persistent directory does not exist. Initializing vector store...")
+
+    # Ensure the text file exists
+    if not os.path.exists(file_path):
+        raise FileNotFoundError(
+            f"The file {file_path} does not exist. Please check the path."
+        )
+    with open(file_path, 'rb') as file:
+        raw_data = file.read()
+        result = chardet.detect(raw_data)
+        encoding = result['encoding']
+
+    print(f"Detected encoding: {encoding}")
+    # Read the text content from the file
+    loader = TextLoader(file_path,encoding=encoding)
+    documents = loader.load()
+
+    # Split the document into chunks
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=100)
+    docs = text_splitter.split_documents(documents)
+
+    # Display information about the split documents
+    print("\n--- Document Chunks Information ---")
+    print(f"Number of document chunks: {len(docs)}")
+    print(f"Sample chunk:\n{docs[0].page_content}\n")
+
+    # Create embeddings
+    print("\n--- Creating embeddings ---")
+    embeddings = GoogleGenerativeAIEmbeddings(
+        model="models/embedding-001"
+    )  # Update to a valid embedding model if needed
+    print("\n--- Finished creating embeddings ---")
+
+    # Create the vector store and persist it automatically
+    print("\n--- Creating vector store ---")
+    db = Chroma.from_documents(
+        docs, embeddings, persist_directory=persistent_directory)
+    print("\n--- Finished creating vector store ---")
+
+else:
+    print("Vector store already exists. No need to initialize.")
+    
+embeddings = GoogleGenerativeAIEmbeddings(
+        model="models/embedding-001"
+    )  # Update to a valid embedding model if needed
+
+# Load the existing vector store with the embedding function
+db = Chroma(persist_directory=persistent_directory,
+            embedding_function=embeddings)
+
+# Define the user's question
+query = "Who is Odysseus' wife?"
+
+# Retrieve relevant documents based on the query
+retriever = db.as_retriever(
+    search_type="similarity_score_threshold",
+    search_kwargs={"k": 3, "score_threshold": 0.4}, #kinda match score type
+    # the higher the threshold like 0.9 its harder to check as presision (simiarity)
+    #k measn the top k results
+)
+relevant_docs = retriever.invoke(query)
+
+# Display the relevant results with metadata
+print("\n--- Relevant Documents ---")
+for i, doc in enumerate(relevant_docs, 1):
+    print(f"Document {i}:\n{doc.page_content}\n")
+    if doc.metadata:
+        print(f"Source: {doc.metadata.get('source', 'Unknown')}\n") #give the source(directory)
